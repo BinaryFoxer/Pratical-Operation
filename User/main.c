0cc500d95799c18cd7adcee6084b2843ec5f28d7
Binary Fox <2628173381@qq.com>
2024-08-14 17:33:15 +0800

    Second version

--- User/main.c
+++ User/main.c
@@ -6,16 +6,18 @@
 #include "Key.h"
 #include "Encoder.h"
 #include "PWM.h"
+#include "myExti.h"
 
 int16_t Count;
 
 int main(){
 	
 	OLED_Init();
-	Key_init();
+//	Key_init();
 	LEDInit();
 	Encoder_Init();
 	PWM_Init();
+	myExti_Init();
 	
 	OLED_ShowString(0, 0, "Count: 0000", OLED_8X16);
 	OLED_Update();
@@ -29,4 +31,16 @@ int main(){
 		Set_CCR(Count);
 	}
 
+}
+
+void EXTI4_IRQHandler(void)
+{
+	if(EXTI_GetITStatus(EXTI_Line4) == SET)
+	{
+		if(EXTI_GetITStatus(EXTI_Line4) == SET)
+		{
+			LED2Turn();
+		}
+	}
+	EXTI_ClearITPendingBit(EXTI_Line4);
 }
